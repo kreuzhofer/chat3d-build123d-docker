1d75dd26734b47e06117e9bd12b86af62ca417ce
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-11-20 16:06:55 +0100

    execute code from string

--- app/main.py
+++ app/main.py
@@ -18,9 +18,15 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/render/")
 def render():
     try:
-        with BuildPart() as box_builder:
-            Box(10, 10, 10)
-            export_step(box_builder.part, "box.step")
+        # Code block to execute as eval from string
+        code_to_execute = """
+with BuildPart() as box_builder:
+    Box(10, 10, 10)
+    export_step(box_builder.part, "box.step")
+"""
+        
+        # Execute the code block
+        exec(code_to_execute)
         
         if os.path.exists("box.step"):
             return FileResponse("box.step", media_type="application/octet-stream", filename="box.step")
